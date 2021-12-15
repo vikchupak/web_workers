510e6237cadf90f7ea9674ed7f1c3a18be373a20
Viktor Chupak <viktor.chupak@danads.se>
2021-12-15 21:39:54 +0200

    log inside worker added

--- src/web.workers/dedicatedWebWorker.js
+++ src/web.workers/dedicatedWebWorker.js
@@ -1,6 +1,7 @@
 import { getNthFibonacciNumberRecursion } from "../utils/get-nth-fibonacci-number-recursion.js";
 
 onmessage = function (event) {
+    console.log("Worker is running")
     const inputValue = event.data;
     const n = getNthFibonacciNumberRecursion(event.data);
     postMessage([inputValue, n])
